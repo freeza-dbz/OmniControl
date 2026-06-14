3b4374cc7eb596757488eb4b65219311e1de432c
Anmol Singh <anmol2006.chauhan@gmail.com>
2026-06-14 14:45:40 +0530

    Add binary files for patch agent and server; update metadata to include project root and AGENT_TOKEN

--- agent/metadata.py
+++ agent/metadata.py
@@ -1,14 +1,22 @@
 import socket
 import platform
 import getpass
+import sys
+import os
+
+# Add the project root to sys.path to allow importing 'shared'
+# This assumes metadata.py is located at OmniControl/agent/metadata.py
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
+
+from shared.auth import ( AGENT_TOKEN )
 
 def get_agent_metadata(device_id: str) -> dict:
-    """
-    Gathers and returns a dictionary of agent metadata.
-    """
+    
     return {
         "device_id": device_id,
         "hostname": socket.gethostname(),
         "username": getpass.getuser(),
         "os": platform.system(),
+        "token" : AGENT_TOKEN
     }
---
+++ shared/auth.py
@@ -0,0 +1,3 @@
+AGENT_TOKEN = "omnicontrol_agent_secret_token"  
+# CONTROLLER_TOKEN = "omnicontrol_controller_secret_token"
+CONTROLLER_TOKEN = "qwerty"  
